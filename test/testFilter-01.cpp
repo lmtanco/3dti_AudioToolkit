ad3791a1e32b56ee70ce607428a9d08d384bf700
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2024-05-20 19:26:04 +0200

    create a first test and scripts to see it

--- test/testFilter-01.cpp
+++ test/testFilter-01.cpp
@@ -2,6 +2,8 @@
 #include <matioCpp/matioCpp.h>
 
 #define BUFFERSIZE 1024
+#define SAMPLINGFREQ 48000
+
 
 int main()
 {
@@ -12,10 +14,23 @@ int main()
     inputBuffer.resize(N);
     outputBuffer.resize(N);
     
+    // Create a delta of value 1 in inputBuffer[0].
+    inputBuffer.Fill(N,0.0);
+    inputBuffer[0] = 1.0;
     
+    // Create a BiQuadFilter
+    Common::CBiquadFilter filter;
+    auto fs = SAMPLINGFREQ;
+    auto fc = 1000;
+    auto Q = 0.707;
+    filter.Setup(fs, fc, Q, Common::T_filterType::LOWPASS);
+
+    
+    // Filter the input with the filter;
+    filter.Process(inputBuffer, outputBuffer);
     
     // Declare a Matio vector
-    matioCpp::Vector<float> vector_in("inpput_vector");
+    matioCpp::Vector<float> vector_in("input_vector");
     vector_in = inputBuffer;
     matioCpp::Vector<float> vector_out("output_vector");
     vector_out = outputBuffer;
