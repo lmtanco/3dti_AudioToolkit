40ed04f735914460b85bff7bfd87ba9b04797e96
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2024-07-20 09:04:31 +0200

    complete test of current ISM implementation

---
+++ scripts/plot_output_log_ism.m
@@ -0,0 +1,59 @@
+function plot_output_log_multiple_9(linear)
+
+% Load the output vectors
+% Inside whe should have 
+% outputLowShelfVector, oputptHighShelfVector, outputBand1Vector, outputBand2Vector, outputBand3Vector, outputBand4Vector, outputBand5Vector, outputBand6Vector, outputBand7Vector
+outputs = open('../build/Debug/test-ism.mat')
+
+% Sampling frequency
+fs = 48000; % in Hz
+
+% Compute the FFT of the vector
+output_fft_left = fft(outputs.left);
+output_fft_right = fft(outputs.right);
+
+% Frequency axis for the FFT plots, up to Nyquist frequency
+n = length(output_fft_left);
+frequencies = (0:n-1)*(fs/n);
+
+% Magnitude of the FFT up to Nyquist frequency and in dB or not depending on parameter linear
+if linear
+    % display message using linear scale
+    disp("Using linear scale")
+    output_fft_magnitude_left = abs(output_fft_left);
+    output_fft_magnitude_right = abs(output_fft_right);
+    mylabel = 'Magnitude (Linear)';
+else
+    % display message using logarithmic scale
+    disp("Using logarithmic scale")
+    output_fft_magnitude_left = 20*log10(abs(output_fft_left));
+    output_fft_magnitude_right = 20*log10(abs(output_fft_right));
+    mylabel = 'Magnitude in DB';
+end
+
+% Create a new figure
+figure;
+
+% Plot everything in the same figure
+semilogx(frequencies, output_fft_magnitude_left, 'LineWidth', 2.5, 'Color', 'k');
+hold on;
+semilogx(frequencies, output_fft_magnitude_right, 'LineWidth', 2.5, 'Color', 'r');
+
+legend( 'Right channel', 'Left channel');
+title('FFT of Output Vectors');
+xlabel('Frequency (Hz - Log Scale)');
+ylabel(mylabel);
+xlim([0 fs/2]); % Limit the x-axis to the Nyquist frequency
+xticks([0, 10, 100, 1000, 10000, fs/2]);
+xticklabels({'', '10', '100', '1000', '10000', num2str(fs/2)});
+
+% if in DB , add 'dB' to the y tick labels
+if ~linear
+    yticks = get(gca, 'ytick');
+    yticklabels = cellstr(num2str(yticks(:)));
+    for i = 1:length(yticklabels)
+        yticklabels{i} = [yticklabels{i} ' dB'];
+    end
+    set(gca, 'yticklabel', yticklabels);
+end
+
--- test/03_ism/testISM.cpp
+++ test/03_ism/testISM.cpp
@@ -1,25 +1,21 @@
-#include "SoundSource.h"
-#include "WavWriter.h"
 #include <BinauralSpatializer/3DTI_BinauralSpatializer.h>
 #include <HRTF/HRTFFactory.h>
 #include <HRTF/HRTFCereal.h>
-#include <BRIR/BRIRFactory.h>
-#include <BRIR/BRIRCereal.h>
 #include <ISM/ISM.h>
-#include <RtAudio.h>
+#include <matioCpp/matioCpp.h>
 
 #include <filesystem>
 namespace fs = std::filesystem;
 
 #define SAMPLERATE 48000
-#define BUFFERSIZE 512
+#define BUFFERSIZE 32768
 #define SOFAFILE "HRTF/SOFA/HRTF_SADIE_II_D1_44100_24bit_256tap_FIR_SOFA_aligned.sofa"
-#define BRIRFILE "BRIR/SOFA/Sala108_listener1_sourceQuad_2m_48kHz_reverb_adjusted.sofa"
 #define ROOMFILE "ISM/XML/trapezoidal_1_A1.xml"
 
 #define LENGTH_OF_NORMALS 0.2
 #define DEFAULT_SCALE 20
 #define INITIAL_REFLECTION_ORDER 3
+#define INITIAL_DIST_SILENCED_FRAMES  1000
 #define FRAME_RATE 60
 
 /// 3dti Toolkit 
@@ -27,12 +23,9 @@ Binaural::CCore							myCore;				 // Core interface
 std::shared_ptr<Binaural::CListener>			listener;			 // Pointer to listener interface
 std::shared_ptr<Binaural::CEnvironment>		environment;
 std::shared_ptr<Binaural::CSingleSourceDSP>  anechoicSourceDSP;   // Pointer to the original source DSP
-SoundSource source1Wav;   
 Common::CTransform sourcePosition;
 Common::CEarPair<CMonoBuffer<float>> outputBufferStereo;
-// void processAnechoic(CMonoBuffer<float> &bufferInput, Common::CEarPair<CMonoBuffer<float>> & bufferOutput);
-// void FillBuffer(CMonoBuffer<float>& output, unsigned int & position, unsigned int & endChunk, std::vector<float>& samplesVector);
-// void audioProcess(Common::CEarPair<CMonoBuffer<float>>& bufferOutput, int bufferSize);
+int numberOfSilencedFrames = 0;
 
 /// ISM
 std::shared_ptr<ISM::CISM>                   ISMHandler;
@@ -40,26 +33,47 @@ ISM::Room                               mainRoom;
 std::vector<shared_ptr<Binaural::CSingleSourceDSP>> imageSourceDSPList;			// Vector of pointers to all image source DSPs
 std::vector<std::vector<float>> absortionsWalls;                                // Vector of wall absorptions
 void LoadRoomGeometryFromXML(fs::path roomPath, ISM::RoomGeometry &roomGeometry, std::vector<std::vector<float>> &absortionsWalls);
-
-// void processImages(CMonoBuffer<float> &bufferInput, Common::CEarPair<CMonoBuffer<float>> & bufferOutput);
-// void processReverb(CMonoBuffer<float> &bufferInput, Common::CEarPair<CMonoBuffer<float>> & bufferOutput);
-
-/// Audio
-std::shared_ptr<RtAudio> audio;
-// int SelectAudioDevice();
-// void LoadWav(std::vector<float>& samplesVector, const char* stringIn);
-// static int rtAudioCallback(void *outputBuffer, void *inputBuffer, unsigned int bufferSize, double streamTime, RtAudioStreamStatus status, void *data);
+std::vector<shared_ptr<Binaural::CSingleSourceDSP>> createImageSourceDSP(); 
+void audioProcess(const CMonoBuffer<float>& input,  Common::CEarPair<CMonoBuffer<float>>& bufferOutput, int bufferSize);
 
 /// setup
 void setup();
 
 int main()
 {
+    // Setup everything:3dti Toolkit core, listener, environment and ISM handler
     setup();
+
+    // Create Delta Input Buffer
+    CMonoBuffer<float> inputBuffer;
+    inputBuffer.resize(BUFFERSIZE, 0.0);
+    inputBuffer[0] = 1.0;
+
+    // Create Ouptut Buffer
+    Common::CEarPair<CMonoBuffer<float>> outputStereoBuffer;
+    outputStereoBuffer.left.resize(BUFFERSIZE);
+    outputStereoBuffer.right.resize(BUFFERSIZE);
+    
+    // Process 
+    audioProcess(inputBuffer, outputStereoBuffer, BUFFERSIZE);
+
+    // Create Matio Vectors
+    matioCpp::Vector<float> leftChannel("left");
+    matioCpp::Vector<float> rightChannel("right");
+    leftChannel = outputStereoBuffer.left;
+    rightChannel = outputStereoBuffer.right;
+
+    // Write to file
+    {
+    matioCpp::File file = matioCpp::File::Create("test-ism.mat");
+    file.write(leftChannel);
+    file.write(rightChannel);
+    }
+
     return 0;
 }
 
-/// @brief  Setup the 3dti Toolkit core, listener, environment and ISM handler, exits if any files are not found
+/// @brief  Setup the 3dti Toolkit core, listener, and ISM handler, exits if any files are not found
 void setup()
 {
     // Retrieve current path
@@ -86,18 +100,6 @@ void setup()
         exit(-1);
     }
 
-    // 3dti Environment
-    environment = myCore.CreateEnvironment();
-    environment->SetReverberationOrder(TReverberationOrder::BIDIMENSIONAL);
-    fs::path brirPath = currentPath / BRIRFILE;
-    if(fs::exists(brirPath)){
-        BRIR::CreateFromSofa(brirPath.string(), environment);
-    }
-    else {
-        std::cout << "BRIR file not found in " << brirPath.string() <<  std::endl;
-        exit(-1);
-    }    
-
     // Room setup
     ISM::RoomGeometry trapezoidal;
     fs::path roomPath = currentPath / ROOMFILE;
@@ -114,7 +116,36 @@ void setup()
     // ISM Handler
     ISMHandler = std::make_shared<ISM::CISM>(&myCore);
     ISMHandler->setupArbitraryRoom(trapezoidal);
+    ISMHandler->setAbsortion(absortionsWalls);
+    ISMHandler->setReflectionOrder(INITIAL_REFLECTION_ORDER);
+    mainRoom = ISMHandler->getRoom(); 
+    ISMHandler->setMaxDistanceImageSources(INITIAL_DIST_SILENCED_FRAMES);
+    numberOfSilencedFrames = ISMHandler->calculateNumOfSilencedFrames(INITIAL_DIST_SILENCED_FRAMES); // I don't think this is necessary
 
+    // Create an impulse source
+    Common::CVector3 sourcePosition = {1.0, 0.0, 0.0};
+    ISMHandler->setSourceLocation(sourcePosition);
+    anechoicSourceDSP = myCore.CreateSingleSourceDSP();
+    Common::CTransform sourceTransform;
+    sourceTransform.SetPosition(sourcePosition);
+    anechoicSourceDSP->SetSourceTransform(sourceTransform);
+    anechoicSourceDSP->SetSpatializationMode(Binaural::TSpatializationMode::NoSpatialization);
+    anechoicSourceDSP->DisableNearFieldEffect();
+    anechoicSourceDSP->DisableAnechoicProcess();
+    anechoicSourceDSP->DisableReverbProcess();
+
+    // Disable all walls but one
+    for (int i = 1; i < mainRoom.getWalls().size(); i++)
+    {
+        ISMHandler->disableWall(i);
+    }
+
+    // Create Image Sources
+    imageSourceDSPList = createImageSourceDSP();
+
+    // Retrieve Image Source Data
+    std::vector<ISM::ImageSourceData> data = ISMHandler->getImageSourceData();
+    
 }
 
 #include <pugixml.hpp>
@@ -184,5 +215,70 @@ void LoadRoomGeometryFromXML(fs::path roomPath, ISM::RoomGeometry &roomGeometry,
     } 
 
 
+
+}
+
+/// @brief The app must create the actual DSP processors of the virtual sources
+std::vector<shared_ptr<Binaural::CSingleSourceDSP>> createImageSourceDSP()
+{
+	std::vector<shared_ptr<Binaural::CSingleSourceDSP>> tempImageSourceDSPList;
+	std::vector<Common::CVector3> imageSourceLocationList = ISMHandler->getImageSourceLocations();
+
+	for (int i = 0; i < imageSourceLocationList.size(); i++)
+	{
+		shared_ptr<Binaural::CSingleSourceDSP> tempSourceDSP = myCore.CreateSingleSourceDSP();								// Creating audio source
+		Common::CTransform sourcePosition;
+		sourcePosition.SetPosition(imageSourceLocationList.at(i));
+		tempSourceDSP->SetSourceTransform(sourcePosition);							//Set source position
+		tempSourceDSP->SetSpatializationMode(Binaural::TSpatializationMode::HighQuality);	// Choosing high quality mode for anechoic processing
+		tempSourceDSP->DisableNearFieldEffect();						// Audio source will not be close to listener, so we don't need near field effect
+		tempSourceDSP->EnableAnechoicProcess();							// Enable anechoic processing for this source
+		tempSourceDSP->DisableDistanceAttenuationAnechoic();			// THIS IS PARTICULAR OF THIS APP
+		tempSourceDSP->EnablePropagationDelay();
+		tempSourceDSP->DisableReverbProcess();   
+		tempImageSourceDSPList.push_back(tempSourceDSP);
+	}
+	return tempImageSourceDSPList;
+}
+
+
+/// @brief  Process the audio buffers
+void audioProcess(const CMonoBuffer<float>& input,  Common::CEarPair<CMonoBuffer<float>>& bufferOutput, int bufferSize)
+{
+    // Assert imageSourceData and imageSourceDSPList have the same size
+    std::vector<ISM::ImageSourceData> imageSourceData = ISMHandler->getImageSourceData();
+    if (imageSourceData.size() != imageSourceDSPList.size())
+    {
+        std::cout << "Error: imageSourceData and imageSourceDSPList have different sizes" << std::endl;
+        exit(-1);
+    }
+
+    // Update listener position
+    Common::CTransform listenerTransform = listener->GetListenerTransform();
+    Common::CVector3 listenerPosition = listenerTransform.GetPosition();
+
+    // Process images
+    std::vector<CMonoBuffer<float>> imageBuffers;
+    ISMHandler->proccess(input, imageBuffers, listenerPosition);
+
+    // Assert imageBuffers and imageSourceDSPList have the same size
+    if (imageBuffers.size() != imageSourceDSPList.size())
+    {
+        std::cout << "Error: imageBuffers and imageSourceDSPList have different sizes" << std::endl;
+        exit(-1);
+    }
+
+    // Process reverb generated by the image sources
+    for (int i = 0; i < imageSourceDSPList.size(); i++)
+    {
+        if (imageSourceData.at(i).visible)
+        {
+            Common::CEarPair<CMonoBuffer<float>> tempBufferOutput;
+            imageSourceDSPList.at(i)->SetBuffer(imageBuffers.at(i));
+            imageSourceDSPList.at(i)->ProcessAnechoic(tempBufferOutput.left, tempBufferOutput.right);
+            bufferOutput.left += tempBufferOutput.left;
+            bufferOutput.right += tempBufferOutput.right;
+        }
+    }
 
 }
