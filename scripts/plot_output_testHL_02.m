44d4a40933d7a60e1ed31cebcbe3e203abf45c6e
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2024-10-29 13:23:26 +0100

    add pink noise test (02)

--- scripts/plot_output_testHL_02.m
+++ scripts/plot_output_testHL_02.m
@@ -1,89 +1,83 @@
 function plot_output_testHL_02()
 
+% Assuming a sampling frequency of 48000 Hz 
+fs = 48000;
+
 inputData = open('../build/Debug/testHL-02-input.mat');
 outputData = open('../build/Debug/testHL-02-output.mat');
 
-% Asumimos que los datos están almacenados en variables llamadas 'inputVector' y 'outputVector'
+% Test assumes datais in 'inputVector' y 'outputVector'
 inputSignal = inputData.inputVector;
 outputSignal = outputData.outputVector;
 
-% Representar las señales en el dominio del tiempo
+% Represent time domain signals
 figure;
+set(gcf, 'Name', 'INPUT / OUTPUT SIGNALS IN TIME DOMAIN');
+time_axis = (0:length(inputSignal)-1)/fs;
 
 subplot(2, 1, 1);
-plot(inputSignal);
-title('Señal de Entrada en el Dominio del Tiempo');
-xlabel('Muestras');
-ylabel('Amplitud');
-% Fix vertical limit to [2,-2]
-ylim([-2, 2]);
+plot(time_axis, inputSignal);
+title('Input signal in time domain');
+xlabel('Time (s)');
+ylabel('Amplitude');
+ylim([-1, 1]); % % Fix vertical limit to [1,-1]
 
 subplot(2, 1, 2);
-plot(outputSignal);
-title('Señal de Salida en el Dominio del Tiempo');
-xlabel('Muestras');
-ylabel('Amplitud');
-% Fix vertical limit to [2,-2]
-ylim([-2, 2]);
+plot(time_axis, outputSignal, 'r');
+title('Output signal in time domain');
+xlabel('Time (s)');
+ylabel('Amplitude');
+% % Fix vertical limit to [1,-1]
+ylim([-1, 1]);
 
-% Calcular la FFT de ambas señales
-inputFFT = fft(inputSignal);
-
-% Calcular la inversa de la respuesta en frecuencia de la señal de entrada
-invInputFFT = 1./inputFFT;
-
-size(invInputFFT)
-
-% Calcular la fft de la salida
-outputFFT = fft(outputSignal);
-
-% Aplicar la inversa de la respuesta en frecuencia de la  entrada a la señal de salida
-responseFFT = oputputFFT.*invInputFFT;
-
-% Calcular el espectro de frecuencias
-n_input = length(inputSignal);
-f_input = (0:n_input-1)*(48000/n_input); % Asumiendo una frecuencia de muestreo de 48000 Hz
-n_output = length(outputSignal);
-f_output = (0:n_output-1)*(48000/n_output); % Asumiendo una frecuencia de muestreo de 48000 Hz
-
-%Visualizar sólo las mitades delos espectros
-half_n_input = floor(n_input/2);
-half_n_output = floor(n_output/2);
-
-% Plot Hsweep
+% Plot input signal in the frequency domain using pwelch
 figure;
-plot(f_input(1:half_n_input), abs(invInputFFT(1:half_n_input)));
-title('Inversa de la Respuesta en Frecuencia Grabada');
-xlabel('Frecuencia (Hz)');
-ylabel('Amplitud');
-xlim([0, 24000]); % Limitar el eje x a la mitad de la frecuencia de muestreo
+% Change name of whole figure
+set(gcf, 'Name', 'INPUT POWER SPECTRUM');
 
-% Representar las señales en el dominio de la frecuencia
+% Pwelch representation of the input signal
+segment_length = round(0.01 * fs); % Segment length (adjustable, e.g., 100 ms)
+noverlap = round(0.5 * segment_length); % Overlap between segments (adjustable)
+nfft = 512; % Number of points for the FFT (adjustable)
+subplot(2, 1, 1);
+inputSignal = double(inputSignal);
+pwelch(inputSignal, segment_length, noverlap, nfft, fs);
+title_str = sprintf('Welch power spectral density estimation with %d ms segments, fft with %d length', segment_length/fs*1000, nfft);
+title(title_str);
+grid on;
+
+% Poctave representation of the input signal
+subplot(2, 1, 2);
+freq_limit = fs/2; % Maximum visualization frequency in Hz
+poctave(inputSignal, fs, 'FrequencyLimits', [30, freq_limit]);
+title_str = sprintf('Octave power spectral density estimation, max freq: %d Hz, one band per octave', freq_limit);
+title(title_str);
+
+% Plot output signal in the frequency domain using pwelch
 figure;
+% Change name of whole figure 
+set(gcf, 'Name', 'OUTPUT POWER SPECTRUM');
+subplot(2, 1, 1);
+outputSignal = double(outputSignal);
+pwelch(outputSignal, segment_length, noverlap, nfft, fs);
+title_str = sprintf('Welch power spectral density estimation with %d ms segments, fft with %d length', segment_length/fs*1000, nfft);
+title(title_str);
+grid on;
+% Use same limits as input signal for vertical axis
+ylim([-90, -50]);
+h = findobj(gca,'Type','Line');
+set(h, 'Color', 'r');
 
-subplot(3, 1, 1);
-plot(f_input(1:half_n_input), abs(inputFFT(1:half_n_input)));
-title('Señal de Entrada en el Dominio de la Frecuencia');
-xlabel('Frecuencia (Hz)');
-ylabel('Amplitud');
-xlim([0, 24000]); % Limitar el eje x a la mitad de la frecuencia de muestreo
+% Poctave representation of the output signal
+subplot(2, 1, 2);
+freq_limit = fs/2; % Maximum visualization frequency in Hz
+poctave(outputSignal, fs, 'FrequencyLimits', [30, freq_limit]);
 
-subplot(3, 1, 2);
-plot(f_output(1:half_n_output), abs(outputFFT(1:half_n_output)));
-title('Señal de Salida en el Dominio de la Frecuencia');
-xlabel('Frecuencia (Hz)');
-ylabel('Amplitud');
-xlim([0, 24000]); % Limitar el eje x a la mitad de la frecuencia de muestreo
-
-
-subplot(3, 1, 3);
-plot(f_output(1:half_n_output), abs(responseFFT(1:half_n_output)));
-title('Respuesta en Frecuencia del Sistema');
-xlabel('Frecuencia (Hz)');
-ylabel('Amplitud');
-xlim([0, 24000]); % Limitar el eje x a la mitad de la frecuencia de muestreo
-
-
+% Change color of the bars in the plot a posteriori to red
+h = findobj(gca,'Type','Bar');
+set(h, 'FaceColor', 'r');
+title_str = sprintf('Octave power spectral density estimation, max freq: %d Hz, one band per octave', freq_limit);
+title(title_str);
 waitfor(gcf);
 
 end
