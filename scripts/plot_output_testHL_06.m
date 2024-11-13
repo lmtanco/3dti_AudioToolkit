71f56ebdb42da3e3bbc9bef3f673c6b0b9ebba3e
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2024-11-13 14:15:53 +0100

    test 06 plots outputs of hearing loss simulation

---
+++ scripts/plot_output_testHL_06.m
@@ -0,0 +1,191 @@
+function plot_output_testHL_06()
+
+inputData = open('../build/Debug/testHL-06-input.mat');
+outputData = open('../build/Debug/testHL-06-output.mat');
+individualFilterOutputData = open('../build/Debug/testHL-06-filterOutputs.mat');
+bandIndicesData = open('../build/Debug/testHL-06-bandIndices.mat');
+hearingLossData = open('../build/Debug/testHL-06-hearingLoss.mat');
+
+% Test assumes data is in 'inputVector' y 'outputVector'
+inputSignal = inputData.inputVector;
+outputSignal = outputData.outputVector;
+
+% Load hearing loss data
+hearingLossLeftSignal = hearingLossData.hearingLossLeft;
+hearingLossRightSignal = hearingLossData.hearingLossRight;
+
+
+% Load individual filter outputs in a cell array
+% Each individual filter is called filterOutputVector_i , i = 00, 01, 02, ...41
+individualFilterOutputs = cell(1, 42);
+for i = 0:41
+    filterOutputName = sprintf('filterOutputVector_%02d', i);
+    individualFilterOutputs{i+1} = individualFilterOutputData.(filterOutputName);
+end
+
+% Load band indices
+bandIndicesStart = bandIndicesData.bandIndicesStart;
+bandIndicesEnd = bandIndicesData.bandIndicesEnd;
+
+% Parameters for pwelch
+segment_length = round(0.01 * 48000); % Segment length (adjustable, e.g., 100 ms)
+noverlap = round(0.5 * segment_length); % Overlap between segments (adjustable)
+nfft = 512; % Number of points for the FFT (adjustable)
+fs = 48000; % Sampling frequency
+
+% Convert signals to double
+inputSignal = double(inputSignal);
+outputSignal = double(outputSignal);
+hearingLossLeftSignal = double(hearingLossLeftSignal);
+hearingLossRightSignal = double(hearingLossRightSignal);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FIGURE 1: POWER SPECTRAL DENSITY USING PWELCH %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate the PSD using pwelch
+[pxx_input, f_input] = pwelch(inputSignal, segment_length, noverlap, nfft, fs);
+[pxx_output, f_output] = pwelch(outputSignal, segment_length, noverlap, nfft, fs);
+
+% Plot the PSD of the input signal
+figure;
+% Change name of whole figure
+set(gcf, 'Name', 'INPUT / OUTPUT POWER SPECTRUM');
+subplot(2, 1, 1);
+f_input_khz = f_input / 1000;
+%plot(f_input_khz, 10*log10(pxx_input), 'b'); % 'b' specifies the color blue
+semilogx(f_input_khz, 10*log10(pxx_input), 'b', 'LineWidth', 2); % 'b' specifies the color blue
+title_str = sprintf('Welch power spectral density estimation (Input) with %d ms segments, fft with %d length', segment_length/fs*1000, nfft);
+title(title_str);
+xlabel('Frequency (kHz)');
+ylabel('Power/Frequency (dB/Hz)');
+grid on;
+%ylim([-90, -50]);
+
+% Plot the PSD of the output signal
+subplot(2, 1, 2);
+f_output_khz = f_output / 1000;
+%plot(f_output_khz, 10*log10(pxx_output), 'r', 'LineWidth', 2); % 'r' specifies the color red
+semilogx(f_output_khz, 10*log10(pxx_output), 'r', 'LineWidth', 2); % 'r' specifies the color red
+title_str = sprintf('Welch power spectral density estimation (Output) with %d ms segments, fft with %d length', segment_length/fs*1000, nfft);
+title(title_str);
+xlabel('Frequency (kHz)');
+ylabel('Power/Frequency (dB/Hz)');
+grid on;
+%ylim([-90, -50]);
+hold on;
+
+% Plot the PSD of the individual filter outputs
+% Create a colormap with a unique color for each band
+numBands = length(bandIndicesStart);
+colormap = turbo(numBands);
+
+for band = 1:numBands
+    for i = bandIndicesStart(band)+1:bandIndicesEnd(band)+1
+        [pxx_filter, f_filter] = pwelch(individualFilterOutputs{i}, segment_length, noverlap, nfft, fs);
+        plot(f_filter/1000, 10*log10(pxx_filter), 'Color', colormap(band, :)); % Use unique color from colormap
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FIGURE 2: TRANSFER FUNCTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Calculate the transfer function (output/input)
+transfer_function = pxx_output ./ pxx_input;
+
+% Plot the transfer function
+figure;
+% Change name of whole figure
+set(gcf, 'Name', 'GAMMATONE FILTER BANK TRANSFER FUNCTION');
+% Use a thicker line for the transfer function
+%plot(f_input_khz, 10*log10(transfer_function), 'k', 'LineWidth', 2); % 'k' specifies the color black, 'LineWidth' sets the thickness
+
+semilogx(f_input_khz, 10*log10(transfer_function), 'k', 'LineWidth', 2); % 'k' specifies the color black, 'LineWidth' sets the thickness
+title_str = sprintf('Gammatone Transfer Function spectral power distribution using Welch with %d ms segments, fft with %d length', segment_length/fs*1000, nfft);
+title(title_str);
+xlabel('Frequency (kHz)');
+ylabel('Magnitude (dB)');
+% Change the x-axis to show the frequency band centers 250, 500, 1000, 2000, 4000, 8000 and 16000 Hz
+% Change the x-axis to show the frequency band centers 125, 250, 500, 1000, 2000, 4000, 8000 and 16000 Hz and also their separation at those values times sqrt(2), in KHz
+set(gca, 'XTick', [0.125, 0.25, 0.5, 1, 2, 4, 8, 16]);
+% Change the xlimits to show up to fs/2
+xlim([0, fs/2]/1000);
+grid on;
+hold on;
+
+% Plot the individual filter transfer functions
+for band = 1:numBands
+    for i = bandIndicesStart(band)+1:bandIndicesEnd(band)+1
+        [pxx_filter, f_filter] = pwelch(individualFilterOutputs{i}, segment_length, noverlap, nfft, fs);
+        transfer_function_filter = pxx_filter ./ pxx_input;
+        plot(f_filter/1000, 10*log10(transfer_function_filter), 'Color', colormap(band, :)); % Use unique color from colormap
+    end
+end
+
+% Represent the octave bands at 125, 250, 500, 1000, 2000, 4000, 8000 and 16000 Hz using the patch function
+% The patch function is used to represent the octave bands in the plot
+% figure;
+% hold on;
+
+% Define the octave bands. FIXME: Read from data generated by test. 
+octave_bands = [125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+% Define the colors for the octave bands using the colormap above
+octave_colors = colormap(1:numBands, :)
+
+% Define the limits of the octave bands
+octave_bands_limits = zeros(2, length(octave_bands));
+for i = 1:length(octave_bands)
+    octave_bands_limits(1, i) = octave_bands(i) / sqrt(2);
+    octave_bands_limits(2, i) = octave_bands(i) * sqrt(2);
+end
+
+% Divide octave_bands_limits by 1000 to convert to kHz
+octave_bands_limits = octave_bands_limits / 1000;
+
+% Define the limits of the plot
+xlimits = get(gca, 'XLim');
+ylimits = get(gca, 'YLim');
+
+% add text to the patches that reads "band %d: %d kHz, band, center frequency"
+for i = 1:length(octave_bands)
+    text(octave_bands(i)/1000, ylimits(1) + 5, sprintf('Band %d:', i));
+    text(octave_bands(i)/1000, ylimits(1) + 2, sprintf(' %d Hz', octave_bands(i)));
+end
+
+% Plot the octave bands
+for i = 1:length(octave_bands)
+    x = [octave_bands_limits(1, i), octave_bands_limits(2, i), octave_bands_limits(2, i), octave_bands_limits(1, i)];
+    y = [ylimits(1), ylimits(1), ylimits(2), ylimits(2)];
+    patch('XData', x, 'YData', y, 'FaceColor', octave_colors(i, :), 'FaceAlpha', 0.2, 'EdgeColor', octave_colors(i, :));
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FIGURE 3: HEARING LOSS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure;
+% Change name of whole figure
+set(gcf, 'Name', 'HEARING LOSS SIMULATION (RIGHT EAR 50 dB LOSS in bands 4 and 5)');
+% Calculate power spectral density using pwelch
+[pxx_hearingLossLeft, f_hearingLossLeft] = pwelch(hearingLossLeftSignal, segment_length, noverlap, nfft, fs);
+[pxx_hearingLossRight, f_hearingLossRight] = pwelch(hearingLossRightSignal, segment_length, noverlap, nfft, fs);
+
+% Calculate transfer function of each ear
+transfer_function_hearingLossLeft = pxx_hearingLossLeft ./ pxx_input;
+transfer_function_hearingLossRight = pxx_hearingLossRight ./ pxx_input;
+
+% Plot the transfer function of the left and right ear
+subplot(1, 2, 1);
+semilogx(f_input_khz, 10*log10(transfer_function_hearingLossLeft), 'b', 'LineWidth', 2); % 'b' specifies the color blue
+title('Hearing Loss Left Ear(no loss)');
+xlabel('Frequency (kHz)');
+ylabel('Magnitude (dB)');
+set(gca, 'XTick', [0.125, 0.25, 0.5, 1, 2, 4, 8, 16]);
+xlim([0, fs/2]/1000);
+
+subplot(1, 2, 2);
+semilogx(f_input_khz, 10*log10(transfer_function_hearingLossRight), 'r', 'LineWidth', 2); % 'r' specifies the color red
+title('Hearing Loss Right Ear(50 dB loss in bands 4 and 5)');
+xlabel('Frequency (kHz)');
+ylabel('Magnitude (dB)');
+set(gca, 'XTick', [0.125, 0.25, 0.5, 1, 2, 4, 8, 16]);
+xlim([0, fs/2]/1000);
+
+
+waitfor(gcf);
+
+end
---
+++ test/04_HL/testHL-06.cpp
@@ -0,0 +1,218 @@
+/**
+ * @file testHL.cpp
+ * @brief 
+ * @version 0.1
+ * @date 2024-10-06
+ * 
+ * @copyright Copyright (c) 2024 Universidad de Málaga
+ * 
+ */
+
+#define SAMPLINGFREQ 48000
+
+
+#define HL_INITIAL_FREQ_HZ 125
+#define HL_BANDS_NUMBER 8
+// These two numbers above give the following bands:
+// 125, 250, 500, 1000, 2000, 4000, 8000, 16000,
+
+
+#define HL_ATTACK_TIME_MS 200
+#define HL_RELEASE_TIME_MS 50
+#define HL_DBS_SPL_FOR_0_DBS_FS 100
+
+#include <HAHLSimulation/GammatoneMultibandExpander.h>  
+#include <HAHLSimulation/HearingLossSim.h>
+#include <Common/Buffer.h>
+#define DR_WAV_IMPLEMENTATION
+#include <dr_wav.h>
+#include <matioCpp/matioCpp.h>
+#include <filesystem>
+
+
+void LoadWav(CMonoBuffer<float> &samplesVector, const char *stringIn);
+
+int main(int argc, char const *argv[])
+{   
+
+    // Obtain the current path 
+    std::filesystem::path currentPath = std::filesystem::current_path();
+
+    // Show the current path
+    std::cout << "Current path is " << currentPath << std::endl;
+
+    std::shared_ptr<HAHLSimulation::CGammatoneMultibandExpander> expander_left = std::make_shared<HAHLSimulation::CGammatoneMultibandExpander>();
+    std::shared_ptr<HAHLSimulation::CGammatoneMultibandExpander> expander_right = std::make_shared<HAHLSimulation::CGammatoneMultibandExpander>();
+
+    // Declare an input buffer
+    CMonoBuffer<float> inputBuffer;
+
+    // Load wav file
+    LoadWav(inputBuffer, "pink_48000_4_sec.wav");
+
+    // Delcare output buffer of same size as input
+    CMonoBuffer<float> outputBuffer(inputBuffer.size());
+
+    // Setup the gammatone multiband expander
+    bool filterGrouping = true; // We will be grouping the filters according to the audiometry band limits
+    expander_left->Setup(SAMPLINGFREQ, HL_INITIAL_FREQ_HZ, HL_BANDS_NUMBER, filterGrouping);
+    expander_right->Setup(SAMPLINGFREQ, HL_INITIAL_FREQ_HZ, HL_BANDS_NUMBER, filterGrouping);
+    // Audiometry band limits.
+    // Note that the band limits are strange, not octaves. 
+    //vector<float> bandLimits = {353.5533906f, 707.1067812f, 1414.213562f, 2449.489743f, 3464.101615f, 4898.979486f, 6928.20323f};
+    vector<float> bandLimits = {176.776695296637f, 353.553390593274f,	707.106781186548f,	1414.21356237310f,	2828.42712474619f,	5656.85424949238f,	11313.7084989848f};
+    //vector<float> bandLimits = {250.0f*sqrtf(2), 500.0f*sqrtf(2), 1000.0f*sqrtf(2), 2000.0f*sqrtf(2), 4000.0f*sqrtf(2), 8000.0f*sqrtf(2), 16000.0f*sqrtf(2)};
+    expander_left->SetGroups(bandLimits);
+    expander_right->SetGroups(bandLimits);
+
+    // Set the attack time for the expanders
+    auto numBands = expander_left->GetNumBands(filterGrouping);
+    for (size_t i = 0; i < numBands; i++)
+    {
+        expander_left->GetBandExpander(i, filterGrouping)->SetAttack(HL_ATTACK_TIME_MS);
+        expander_left->GetBandExpander(i, filterGrouping)->SetRelease(HL_RELEASE_TIME_MS);
+
+        expander_right->GetBandExpander(i, filterGrouping)->SetAttack(HL_ATTACK_TIME_MS);
+        expander_right->GetBandExpander(i, filterGrouping)->SetRelease(HL_RELEASE_TIME_MS);
+    }
+
+    // Process the input buffer
+    expander_left->Process(inputBuffer, outputBuffer);
+
+    // Access the individual filters process the input individually for each filter
+    Common::CGammatoneFilterBank &filterBank = expander_left->GetGammatoneFilterBank();
+    auto numFilters = filterBank.GetNumFilters();
+    std::vector<CMonoBuffer<float>> filterOutputs;
+    for (auto i = 0; i < numFilters; i++)
+    {
+        CMonoBuffer<float> filterOutput(inputBuffer.size());
+        auto filter = filterBank.GetFilter(i);
+        filter->Process(inputBuffer, filterOutput);
+        filterOutputs.push_back(filterOutput);
+        // Acces the filter frequency and bandwith for each filter
+        auto freq = filter->GetCenterFrequency();
+        auto bw = filter->GetERBBandwidth();
+    }
+  
+
+    // Copy the input and output to matio vectors
+    matioCpp::Vector<float> inputVector("inputVector");
+    matioCpp::Vector<float> outputVector("outputVector");
+    inputVector = inputBuffer;
+    outputVector = outputBuffer;
+
+    // Copy the individual filter outputs to matio vectors
+    std::vector<matioCpp::Vector<float>> filterOutputVectors;
+    for (auto i = 0; i < numFilters; i++)
+    {
+        std::ostringstream oss;
+        oss << "filterOutputVector_" << std::setw(2) << std::setfill('0') << i;
+        matioCpp::Vector<float> filterOutputVector(oss.str());
+        filterOutputVector = filterOutputs[i];
+        filterOutputVectors.push_back(filterOutputVector);
+    }
+
+    // Save the input and output to a mat file
+    {
+        matioCpp::File file = matioCpp::File::Create("testHL-06-input.mat");
+        file.write(inputVector);
+    }
+    {
+        matioCpp::File file = matioCpp::File::Create("testHL-06-output.mat");
+        file.write(outputVector);
+    }
+    // Save the individual filter outputs to a mat file
+    {
+        matioCpp::File file = matioCpp::File::Create("testHL-06-filterOutputs.mat");
+        for (auto i = 0; i < numFilters; i++)
+        {
+            file.write(filterOutputVectors[i]);
+        }
+    }
+
+    // Save the band group indices to a .mat file
+    matioCpp::Vector<int> bandIndicesStart("bandIndicesStart");
+    matioCpp::Vector<int> bandIndicesEnd("bandIndicesEnd");
+    std::vector<int> startIndices, endIndices;
+    for (const auto& indices : expander_left->GetBandIndices()) {
+        startIndices.push_back(indices[0]);
+        endIndices.push_back(indices[1]);
+    }
+    bandIndicesStart = startIndices;
+    bandIndicesEnd = endIndices;
+
+    {
+        matioCpp::File file = matioCpp::File::Create("testHL-06-bandIndices.mat");
+        file.write(bandIndicesStart);
+        file.write(bandIndicesEnd);
+    }
+
+    // Declare a Hearing Loss Simulation object
+    HAHLSimulation::CHearingLossSim hearingLossSim;
+
+    // Setup the hearing loss simulation with dB SPL for 0 dB FS equal to 100 dB SPL for the sake of testing.
+    // The last parameter (bufferSize) is used to initialize the frequency smearing. 
+    // Not sure if this is the correct use as I am passing the whole input (the 4 seconds of pink noise)
+    hearingLossSim.Setup(SAMPLINGFREQ, HL_DBS_SPL_FOR_0_DBS_FS, HL_BANDS_NUMBER, inputBuffer.size());    
+
+    // Connect simulator and expanders
+    hearingLossSim.SetMultibandExpander(Common::T_ear::LEFT, expander_left);
+    hearingLossSim.SetMultibandExpander(Common::T_ear::RIGHT, expander_right);
+
+    // Declare a vector to store the audiometry values
+    std::vector<float> audiometryNoLoss = {0,0,0,0,0,0,0,0};
+    std::vector<float> audiometryCentralLoss = {0,0,0,50,50,0,0,0};
+
+    // Set the audiometry values
+    hearingLossSim.SetFromAudiometry_dBHL(Common::T_ear::LEFT, audiometryNoLoss);
+    hearingLossSim.SetFromAudiometry_dBHL(Common::T_ear::RIGHT, audiometryCentralLoss);
+
+    // Create a stereo buffer as input
+    Common::CEarPair<CMonoBuffer<float>> stereoInputBuffer{inputBuffer, inputBuffer};
+
+    // Create a stereo buffer as output
+    Common::CEarPair<CMonoBuffer<float>> stereoOutputBuffer;
+    stereoOutputBuffer.left.resize(inputBuffer.size());
+    stereoOutputBuffer.right.resize(inputBuffer.size());  
+
+    // Process the stereo buffer
+    hearingLossSim.Process(stereoInputBuffer, stereoOutputBuffer);
+
+
+    // Copy the stereo output to matio vectors
+    matioCpp::Vector<float> stereoOutputLeft("hearingLossLeft");
+    matioCpp::Vector<float> stereoOutputRight("hearingLossRight");
+    stereoOutputLeft = stereoOutputBuffer.left;
+    stereoOutputRight = stereoOutputBuffer.right;
+
+    // Save the stereo output to a mat file
+    {
+        matioCpp::File file = matioCpp::File::Create("testHL-06-hearingLoss.mat");
+        file.write(stereoOutputLeft);
+        file.write(stereoOutputRight);
+    }   
+    
+    return 0;
+}
+
+#include <iostream>
+
+void LoadWav(CMonoBuffer<float> &samplesVector, const char *stringIn)
+{
+    unsigned int channels;
+    unsigned int sampleRate;
+    drwav_uint64 totalPCMFrameCount;
+    float *pSampleData = drwav_open_file_and_read_pcm_frames_f32(stringIn, &channels, &sampleRate, &totalPCMFrameCount, NULL);
+    if (pSampleData != NULL) {
+        for (size_t i = 0; i < totalPCMFrameCount; i++)
+        {
+            samplesVector.push_back(pSampleData[i]);
+        }
+        drwav_free(pSampleData, NULL);
+    }
+    else
+    {
+        std::cerr << "Error loading wav file" << std::endl;
+        std::exit(-1);
+    }
+}
